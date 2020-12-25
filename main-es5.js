08bfb6de5dba9bdbf4015cc575b53bd61c13358e
Daniel Fesalbon <35003912+danielfesalbon@users.noreply.github.com>
2020-12-25 20:30:11 +0800

    Update main-es5.js

--- main-es5.js
+++ main-es5.js
@@ -397,7 +397,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
           _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](14, "div", 3);
 
-          _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](15, "and I'm wondering how the fuck did you get here.");
+          _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](15, "and I'm wondering how did you get here.");
 
           _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
 
